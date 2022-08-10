84443d7361b8197da1824e740e2d20703f241e6c
Manuel Stör <m.stoer@microstream.one>
2022-08-10 13:39:37 +0200

    Test different root

--- src/main/java/com/example/DataRoot.java
+++ src/main/java/com/example/DataRoot.java
@@ -1,9 +1,33 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataRoot
 {
-	public final List<String> myStrings  = new ArrayList<>();
+	public static class MyClass
+	{
+		private Map<String, String> map = new HashMap<>();
+
+		public Map<String, String> getMap()
+		{
+			return this.map;
+		}
+
+		public void setMap(final Map<String, String> map)
+		{
+			this.map = map;
+		}
+
+		public MyClass(final Map<String, String> map)
+		{
+			this.map = map;
+		}
+
+		public MyClass()
+		{
+		}
+	}
+
+	public final MyClass myStrings = new MyClass();
 }
