2623fc45f2bc8656c8440d29455686276f5c124f
Manuel Stör <m.stoer@microstream.one>
2023-05-31 15:08:44 +0200

    Test microstream path changes

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import io.micronaut.runtime.Micronaut;
+import one.microstream.enterprise.cluster.nodelibrary.micronaut.MicronautMicrostreamClusterController;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+    public static void main(final String[] args) {
+        final var ctx = Micronaut.run(Application.class, args);
+        new MicronautMicrostreamClusterController().load(ctx);
     }
 }
