5101aa883052dd1d6a3b66372476ac5c696d2c0b
Manuel Stör <m.stoer@microstream.one>
2023-06-01 08:18:49 +0200

    remove stuff

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,10 @@
 package com.example;
 
 import io.micronaut.runtime.Micronaut;
-import one.microstream.enterprise.cluster.nodelibrary.micronaut.StorageClusterController;
 
 public class Application {
 
     public static void main(final String[] args) {
-        final var ctx = Micronaut.run(Application.class, args);
-        ctx.inject(new StorageClusterController());
+        Micronaut.run(Application.class, args);
     }
 }
