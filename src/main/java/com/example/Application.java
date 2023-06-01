dc94aaafe8cd9963f38e7d5c98e06758cdf7d41e
Manuel Stör <m.stoer@microstream.one>
2023-06-01 07:36:29 +0200

    Use new microstream cluster controller

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,12 @@
 package com.example;
 
 import io.micronaut.runtime.Micronaut;
-import one.microstream.enterprise.cluster.nodelibrary.micronaut.MicronautMicrostreamClusterController;
+import one.microstream.enterprise.cluster.nodelibrary.micronaut.StorageClusterController;
 
 public class Application {
 
     public static void main(final String[] args) {
         final var ctx = Micronaut.run(Application.class, args);
-        new MicronautMicrostreamClusterController().load(ctx);
+        new StorageClusterController().load(ctx);
     }
 }
--- src/main/java/com/example/MicrostreamController.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import io.micronaut.http.annotation.Controller;
-import one.microstream.enterprise.cluster.nodelibrary.micronaut.StorageClusterController;
-
-@Controller("/my-microstream-path")
-public class MicrostreamController extends StorageClusterController
-{
-}
