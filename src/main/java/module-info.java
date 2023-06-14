dfda92f59f3c6170d2b6de658cf26e3c1dc6b213
Manuel Stör <m.stoer@microstream.one>
2023-06-14 07:51:06 +0200

    idimf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module myClusterApp
+module MyClusterApp
 {
 	exports com.example;
 
@@ -6,7 +6,6 @@ module myClusterApp
 	requires io.micronaut.core;
 	requires io.micronaut.http;
 	requires io.micronaut.inject;
-	requires jakarta.inject;
 	requires microstream.afs;
 	requires microstream.base;
 	requires microstream.enterprise.cluster.nodelibrary;
